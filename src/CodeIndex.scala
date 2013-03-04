8815fc03c5ec330c8e6363e454d764e09bbc8a4e
Chris Sphinx <chrissphinx@gmail.com>
2013-03-04 05:01:13 -0500

    waiting for answers on specs

--- src/CodeIndex.scala
+++ src/CodeIndex.scala
@@ -1,24 +1,35 @@
 class CodeIndex(whichHF: String, whichCRA: String, MAX_N_HOME_LOC: Int) {
-	val ARRAY_SIZE = 300
+	val ARRAY_SIZE = 440
 	val countryCodes = Array.ofDim[String](ARRAY_SIZE)
 	val links = Array.ofDim[String](ARRAY_SIZE)
 	val pw = new java.io.PrintWriter(
 			 new java.io.FileWriter("log.txt", true), true)
 
 	def insert(code: String) {
-		insert(code, hash(code, whichHF)
+		println("CODE: " + code + "\tHASH: " + hash(code, whichHF))
+		insert(code, hash(code, whichHF))
 	}
 
 	def finish(print: Boolean) {
 		if(print) {
 			pw.println("PRINTING HASH TABLE")
 		} else pw.println("JUST LOGGING AVERAGE SEARCH PATH")
+			println(countryCodes.mkString("\n"))
+			println(whichHF)
 	}
 
 	private def insert(code: String, loc: Int) {
 		whichCRA match {
-			case "linEmb" => 
-			case "chSep" => 
+			case "linEmb" => if(countryCodes(loc) == null) countryCodes(loc) = code
+							 else if(loc == MAX_N_HOME_LOC) insert(code, 0)
+							 else insert(code, loc + 1)
+
+			case "chSep" =>  // println("INSERTING: " + code + " " + loc)
+							 if(countryCodes(loc) == null) countryCodes(loc) = code
+							 else {
+							 	if(loc < MAX_N_HOME_LOC) insert(code, MAX_N_HOME_LOC)
+							 	else insert(code, loc + 1)
+							 }
 		}
 	}
 
